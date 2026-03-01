6d7135982efad5aba24daffbe30d24281d81e6dd
adel141 <mithilahakim7@gmail.com>
2026-03-01 14:21:56 +0600

    js lab  class1

---
+++ jslabprac/js_Practice.php
@@ -0,0 +1,38 @@
+<DOCTYPE html>  
+<html>
+<head>
+    <title>JavaScript Class</title>
+</head>
+<body>
+    <center>
+        <p>Welcome to JavaScript Class</p>
+    </center>
+
+
+    <script>
+    var name = "John Doe";
+    var age = 30;
+    var is_human = true;
+    var x;
+    var y = null;
+    var name2 = "mr. rahim";
+    let name3 = 80.0007;
+    console.log("Welcome to JavaScript Class");
+    // alert("Welcome to JavaScript Class");
+    console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof is_human);
+    console.log(typeof x);
+    console.log(typeof y);
+    console.log(typeof name2);
+    console.log(typeof name3);
+    var num1=40;
+    var num2=60;
+    console.log("The sum is: ",num1+num2);
+    console.log("The sub is: ",num1-num2);
+    console.log("The mul is: ",num1*num2);
+    console.log("The div is: ",num1/num2);
+
+
+    </script>
+</body>
