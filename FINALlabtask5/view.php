ad421ac522444bfab3095c52987ae9f2a2f2102e
adel141 <mithilahakim7@gmail.com>
2026-05-05 08:40:37 +0600

    edit modified

--- FINALlabtask5/view.php
+++ FINALlabtask5/view.php
@@ -13,8 +13,9 @@ $result = mysqli_query($conn, "SELECT * FROM students");
 
 <h2>Student List</h2>
 
-<table border="1" cellpadding="10">
+<table border="1" >
 <tr>
+    <th>ID</th>
     <th>Name</th>
     <th>Email</th>
     <th>Reg No</th>
